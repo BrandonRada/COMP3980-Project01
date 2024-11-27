de8e1e1842b9c9aebbc59c18da7e9fe58247eb6c
Brandon Rada <brandonn.rada@gmail.com>
2024-11-26 18:06:38 -0800

    Moved input handling into handle_input files, moved the player struct into a Player.h file.
    NOTE:
    For whatever reason, linking the Player.h file in files.txt, gives errors stating that the variables (player_char, x, y) held in the player struct are never used.
    I am leaving it out of files.txt for now

--- source/include/HandleInput.h
+++ source/include/HandleInput.h
@@ -6,9 +6,10 @@
 #define MAIN_HANDLEINPUT_H
 
 // #include "../src/main.c"
+#include "./Player.h"
 #include <SDL2/SDL.h>
 #define MOVEMENT_THRESHOLD 8000
 
-// void handle_input(SDL_GameController *controller, SDL_Event *event, struct player *my_player, int min_x, int min_y, int max_x, int max_y);
+void handle_input(SDL_GameController *controller, SDL_Event *event, struct player *my_player, int min_x, int min_y, int max_x, int max_y);
 
 #endif    // MAIN_HANDLEINPUT_H
---
+++ source/include/Player.h
@@ -0,0 +1,11 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+struct player
+{
+    const char *player_char;
+    int         x;
+    int         y;
+};
+
+#endif    // MAIN_PLAYER_H
